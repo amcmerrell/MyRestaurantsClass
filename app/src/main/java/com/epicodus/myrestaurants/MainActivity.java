c65e2363516f32ad7f1a98739cc36e30c9f1cf6b
Andrew Merrell and Nhat Hoang <andrew@>
2016-11-14 14:55:34 -0800

    change font to my Restaurant.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,18 +1,22 @@
 package com.epicodus.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
     private Button mGoToAboutButton;
+    private TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mAppNameTextView.setTypeface(pacificoFont);
 
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
