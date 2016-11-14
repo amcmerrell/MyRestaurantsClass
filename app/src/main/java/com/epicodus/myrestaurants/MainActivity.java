625dd124a85b2f0aff4968e00f3a711940557ff3
Andrew Merrell and Nhat Hoang <andrew@>
2016-11-14 11:41:00 -0800

    add about page.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -12,12 +12,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private Button mGoToAboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", location);
                 startActivity(intent);
+            }
+        });
+
+        mGoToAboutButton = (Button) findViewById(R.id.goToAboutButton);
+        mGoToAboutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent aboutIntent = new Intent(MainActivity.this, AboutRestaurants.class);
+                startActivity(aboutIntent);
             }
         });
     }
